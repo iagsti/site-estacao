b3a0bde53ee5aebf4d24ed89f2ebadb80526d422
Marcelo Schneider <schneider.fei@gmail.com>
2020-05-13 18:19:01 -0300

    Refatoração para utilização da api - issue #10 - Refatoração do código para utilização da api de dados meteorológicos

--- estacao/core/tests/test_core_home.py
+++ estacao/core/tests/test_core_home.py
@@ -1,16 +1,10 @@
 from django.test import TestCase
 from django.utils.translation import gettext as _
 from django.shortcuts import resolve_url as r
-from estacao.core.models import Dados
 
 
 class HomeGetTest(TestCase):
-
-    databases = ['dados']
-
     def setUp(self):
-        data = self.make_data()
-        Dados.objects.create(**data)
         self.resp = self.client.get(r('core:home'))
 
     def test_status_code(self):
@@ -40,7 +34,7 @@ class HomeGetTest(TestCase):
     def test_menu_content(self):
         """Template should render a list of menu itens"""
         menu_itens = ['Home', _('Contato e Localização'), _('Pessoal')]
-        
+
         for item in menu_itens:
             with self.subTest():
                 self.assertContains(self.resp, item)
@@ -48,31 +42,8 @@ class HomeGetTest(TestCase):
     def test_page_title(self):
         """Template should render page title"""
         content = ((2, _('Estação Meteorológica')), (1, _('Seção Técnica de Serviços Meteorológicos')),
-                    (1, _('Instituto de Astronomia, Geofísica e Ciências Atmosféricas')))
+                   (1, _('Instituto de Astronomia, Geofísica e Ciências Atmosféricas')))
 
         for count, expected in content:
             with self.subTest():
                 self.assertContains(self.resp, expected, count)
-
-    def test_content(self):
-        """Template should render content"""
-        content = [_('Condições Atuais'), _('Meteograma'), _('Boletins e Relatórios'),
-                    _('Museu de Meteorologia'), _('Instrumentos'), _('Solicitação de Dados'),
-                    _('Explorando a Meteorologia'), _('Cursos')]
-
-        for expected in content:
-            with self.subTest():
-                self.assertContains(self.resp, expected)
-
-    def make_data(self, **kwargs):
-        default_data = {"data" : "1995-02-10 17:00:00", "vis" : 7, "tipob" : "Sc/Cu",
-		                "qtdb" : 5, "tipom" : "Ac", "qtdm" : 3, "tipoa" : 'Ac',
-		                "qtda" : 0, "dir" : "WNW", "vento" : 3, "temp_bar" : 24,
-		                "pressao" : 695.5, "tseco" : 25.4, "tumido" : 21, "tsfc" : 28.5,
-		                "t5cm" : 27, "t10cm" : 26.5, "t20cm" : 25, "t30cm" : 23.3,
-		                "t40cm" : 23.2, "piche" : 4, "evap_piche" : 4, "piche_ar" : 4.5,
-		                "evap_piche_ar" : 4.5, "tmax" : 18, "tmin" : 0}
-
-        data = dict(default_data, **kwargs)
-        return data
-    
--- estacao/core/views.py
+++ estacao/core/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
-from estacao.core.models import Dados
 
 
 def home(request):
-    conditions = make_conditions(Dados.objects)
-    context = {'conditions': conditions}
+    context = {'conditions': ''}
     return render(request, 'home.html', context)
-
-
-def make_conditions(conditions):
-    dados = Dados.objects.max_temperature()
-    ur = Dados.objects.relative_humidity()
-    td = Dados.objects.td()
-    p_hpa = Dados.objects.p_hpa()
-    visibility = Dados.objects.visibility()
-    nuvens_baixas = Dados.objects.nuvens_baixas()
-    nuvens_medias = Dados.objects.nuvens_medias()
-    nuvens_altas = Dados.objects.nuvens_altas()
-
-    return {'temperatura_ar': dados.tseco, 'ur': ur, 'temperatura_orvalho': td,
-            'temperatura_min': dados.tmin, 'temperatura_max': dados.tmax,
-            'vento': dados.vento, 'dir': dados.dir, 'pressao': p_hpa,
-            'visibilidade': visibility, 'nuvens_baixas': nuvens_baixas,
-            'nuvens_medias': nuvens_medias, 'nuvens_altas': nuvens_altas,
-            'diversos': 'Não disponível'}
