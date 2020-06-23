0ffc651ab6e3bb326242116bc54945b1e64c7a83
Marcelo Schneider <schneider.fei@gmail.com>
2020-06-23 15:06:06 -0300

    Refatora utilizando  decorators - issue #17

--- estacao/core/tests/test_core_charts.py
+++ estacao/core/tests/test_core_charts.py
@@ -1,8 +1,7 @@
-import json
-import httpretty
 from django.test import TestCase
 from estacao.core.charts.temperature import Temperature
 from estacao.core.resources import MeteogramTemperature
+from estacao.core.tests.mock import mock_meteogram_request
 
 API_URI = 'http://api.estacao.iag.usp.br/v0/2020-01-01/2020-02-01/'
 
@@ -19,9 +18,10 @@ class TemperatureTest(TestCase):
         """It should have load_data attribute"""
         self.assertTrue(hasattr(self.temp, 'load_data'))
 
+    @mock_meteogram_request
     def test_load_data(self):
         """It should get temperature data"""
-        self.set_mock(self.temp.plot)
+        self.temp.plot()
         self.assertDictEqual(self.temp.temperature_data, self.make_data())
 
     def test_has_plot_attribute(self):
@@ -39,39 +39,35 @@ class TemperatureTest(TestCase):
         self.temp.extract_data(self.make_data())
         self.assertDictEqual(expected, self.temp.extracted_data)
 
+    @mock_meteogram_request
     def test_generate_components(self):
-        self.set_mock(self.temp.plot)
+        self.temp.plot()
         self.temp.generate_components()
         for expected in ['script', 'div']:
             message = '{} not found'.format(expected)
             with self.subTest():
                 self.assertTrue(hasattr(self.temp, expected), msg=message)
 
+    @mock_meteogram_request
     def test_plot(self):
-        self.set_mock(self.temp.plot)
+        self.temp.plot()
         self.assertIn('text/javascript', self.temp.script)
         self.assertIn('class', self.temp.div)
 
+    @mock_meteogram_request
     def test_get_script(self):
-        self.set_mock(self.temp.plot)
+        self.temp.plot()
         self.assertIn('text/javascript', self.temp.get_script())
 
+    @mock_meteogram_request
     def test_get_div(self):
-        self.set_mock(self.temp.plot)
+        self.temp.plot()
         self.assertIn('class', self.temp.get_div())
 
     def test_generate_uri(self):
         expected = API_URI
         self.temp.generate_uri('2020-01-01', '2020-02-01')
         self.assertEqual(expected, self.temp.uri)
-
-    @httpretty.httprettified
-    def set_mock(self, function):
-        uri = API_URI
-        body = json.dumps(self.make_data())
-        httpretty.register_uri(httpretty.GET, uri=uri, body=body)
-        data = function(date_ini='2020-01-01', date_end='2020-02-01')
-        return data
 
     def make_data(self):
         data = {
