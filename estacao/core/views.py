b7a6f807d1f95e55f964001b0910154c9a70f889
Marcelo Schneider <schneider.fei@gmail.com>
2020-06-25 21:12:20 -0300

    Refatora a função home - issue #17

--- estacao/core/views.py
+++ estacao/core/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from bokeh.resources import CDN
 from estacao.core.resources import WeatherResource
 from estacao.core.charts.temperature import Temperature
 
 
 def home(request):
-    temp = Temperature()
-    temp.plot()
+    temp = render_temperature_chart()
+    weather_data = get_weather_data()
+    context = {
+        'conditions': weather_data,
+        'temperature': temp,
+        'bokehcdn': CDN.render()
+    }
+    return render(request, 'home.html', context)
+
+
+def get_weather_data():
     weather = WeatherResource()
     weather_data = weather.get_weather_data()
-    context = {'conditions': weather_data.json(), 'temperature': temp}
-    return render(request, 'home.html', context)
+    return weather_data
+
+
+def render_temperature_chart():
+    temperature = Temperature()
+    temperature.plot()
+    return temperature
