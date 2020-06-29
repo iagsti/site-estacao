f729dd22cab7973ec215b9227394aa068b1a77db
Marcelo Schneider <schneider.fei@gmail.com>
2020-06-29 19:04:31 -0300

    Adiciona classe UriManager - issue #21

--- estacao/core/resources.py
+++ estacao/core/resources.py
@@ -1,10 +1,30 @@
-from django.conf import settings
 import requests
+import numpy as np
+from os import path
+from urllib.parse import urljoin
+from django.conf import settings
+
+
+class UriManager:
+    def generate_uri(self, step=1, version='v0', resource=''):
+        self.make_date_range(step=step)
+        self.generate_path(version=version, resource=resource)
+        url = getattr(settings, 'API_URL')
+        return urljoin(url, self.path)
+
+    def generate_path(self, version='v0', resource=''):
+        self.path = path.join(version, resource, self.date_range)
+
+    def make_date_range(self, step=1):
+        today = np.datetime64('today')
+        date_delta = np.timedelta64(step, 'D')
+        yesterday = today - date_delta
+        self.date_range = path.join(str(yesterday), str(today))
 
 
 class WeatherResource:
     def __init__(self):
-        self.uri = settings.API_URL
+        self.uri = getattr(settings, 'API_URL')
 
     def get_weather_data(self):
         try:
@@ -28,16 +48,16 @@ class WeatherResource:
         return response
 
 
-class MeteogramTemperature:
+class MeteogramTemperature(UriManager):
     def __init__(self):
         self.uri = settings.API_URL
 
     def temperature_min(self):
-        url = self.uri + '/temperature_min'
-        response = requests.get(url)
-        return response
+        uri = self.generate_uri(resource='temperature_min')
+        response = requests.get(uri)
+        return response.json()
 
     def temperature_max(self):
-        url = self.uri + '/temperature_max'
-        response = requests.get(url)
-        return response
+        uri = self.generate_uri(resource='temperature_max')
+        response = requests.get(uri)
+        return response.json()
