863b487f837fb79d03b863fddcc99389cced7399
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-16 15:51:07 -0300

    Adiciona os métodos plot e make_components - issue #21

--- estacao/core/charts/temperature.py
+++ estacao/core/charts/temperature.py
@@ -5,6 +5,13 @@ from .plot import PlotTemperature
 
 
 class Temperature():
+    def plot(self):
+        self.make_plots()
+        self.make_components()
 
+    def make_components(self):
+        plot = layout([self.graph], sizing_mode='stretch_width')
+        script, div = components(plot)
+        self.components = {'script': script, 'div': div}
 
 
