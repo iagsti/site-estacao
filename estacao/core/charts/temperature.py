ec10764fca54f7704c2233a855cd8cab59e48729
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-16 15:47:34 -0300

    Remove os métodos make_uri e extract_data - issue #21

--- estacao/core/charts/temperature.py
+++ estacao/core/charts/temperature.py
@@ -1,16 +1,6 @@
 
 
-    def make_uri(self):
-        self.uri = self.generate_uri(resource='temperatura-min')
 
-    def extract_data(self, data):
-        try:
-            extracted = dict()
-            extracted['date'] = [item['data'] for item in data['temp_min']]
-            extracted['temp_min'] = [item['temp'] for item in data['temp_min']]
-        except KeyError:
-            extracted = {'date': ['2020-06-25 13:00:00'], 'temp_min': ['0']}
-        self.extracted_data = extracted
 class Temperature():
 
 
