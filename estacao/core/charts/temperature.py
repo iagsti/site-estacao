7138b43b9ebe5e80dbfea8ea48f7abdc5e933448
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-16 15:52:15 -0300

    Adiciona o método make_plots - issue #21

--- estacao/core/charts/temperature.py
+++ estacao/core/charts/temperature.py
@@ -14,4 +14,8 @@ class Temperature():
         script, div = components(plot)
         self.components = {'script': script, 'div': div}
 
+    def make_plots(self):
+        temp = PlotTemperature()
+        temp.generate_graph()
+        self.graph = temp.graph
 
