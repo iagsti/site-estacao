09120644001ea1c31d5aea2b9b4807a59daf6af3
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-16 15:39:15 -0300

    Remove a classe TemperatureMax - issue #21

--- estacao/core/charts/temperature.py
+++ estacao/core/charts/temperature.py
@@ -15,15 +15,4 @@ class Temperature(TempChart):
         self.extracted_data = extracted
 
 
-class TemperatureMax(TempChart):
-    def make_uri(self):
-        self.uri = self.generate_uri(resource='temperatura-max')
 
-    def extract_data(self, data):
-        try:
-            extracted = dict()
-            extracted['date'] = [item['data'] for item in data['temp_max']]
-            extracted['temp_max'] = [item['temp'] for item in data['temp_max']]
-        except KeyError:
-            extracted = {'date': ['2020-06-25 13:00:00'], 'temp_max': ['0']}
-        self.extracted_data = extracted
