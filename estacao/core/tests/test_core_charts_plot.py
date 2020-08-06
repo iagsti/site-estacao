4417f59b0f7f29c89f9267c6b337c85c536c1f47
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-06 12:28:51 -0300

    Modifica os testes para PlotTemperature - issue #25
    Altera os testes para as modificações da classe PlotTemperature.

--- estacao/core/tests/test_core_charts_plot.py
+++ estacao/core/tests/test_core_charts_plot.py
@@ -1,44 +1,64 @@
 from django.test import TestCase
-from estacao.core.charts.plot import PlotTemperature
-from .mock import temperature_max, temperature_min, mock_api, to_datetime
+from bokeh.plotting.figure import Figure
+from .mock import temperature_max, temperature_min, mock_api
+
+from estacao.core.charts.plot import TemperaturePlot
 
 
-class PlotTemperatureTest(TestCase):
+def mock_loaded_data():
+    data_max = temperature_max
+    date_max = [item['data'] for item in data_max['temp_max']]
+    temp_max = [item['temp'] for item in data_max['temp_max']]
+
+    data_min = temperature_min
+    date_min = [item['data'] for item in data_min['temp_min']]
+    temp_min = [item['temp'] for item in data_min['temp_min']]
+
+    return {
+        'temp_max': {'date': date_max, 'temp_max': temp_max},
+        'temp_min': {'date': date_min, 'temp_min': temp_min}
+    }
+
+
+class TemperaturePlotTest(TestCase):
     def setUp(self):
-        self.obj = PlotTemperature()
-
-    def test_has_load_temperature(self):
-        self.assertTrue(hasattr(self.obj, 'load_temperature'))
+        self.obj = TemperaturePlot()
 
     @mock_api
     def test_load_temperature_data(self):
-        """It should set data attribute"""
-        data_max = temperature_max
-        date_max = [item['data'] for item in data_max['temp_max']]
-        date_max = to_datetime(date_max)
-        temp_max = [item['temp'] for item in data_max['temp_max']]
-
-        data_min = temperature_min
-        date_min = [item['data'] for item in data_min['temp_min']]
-        date_min = to_datetime(date_min)
-        temp_min = [item['temp'] for item in data_min['temp_min']]
-
-        expected = {
-            'temp_max': {'date': date_max, 'temp_max': temp_max},
-            'temp_min': {'date': date_min, 'temp_min': temp_min}
-        }
-        self.maxDiff = None
-        self.obj.load_temperature('temperatura-max', 'temp_max')
-        self.obj.load_temperature('temperatura-min', 'temp_min')
+        expected = mock_loaded_data()
+        self.obj.load_temperature_data()
         self.assertDictEqual(expected, self.obj.data)
 
     @mock_api
-    def test_make_data_source(self):
-        data_max = temperature_max
-        date = [item['data'] for item in data_max['temp_max']]
-        date = to_datetime(date)
-        temp = [item['temp'] for item in data_max['temp_max']]
-        expected = {'date': date, 'temp': temp}
-        self.obj.load_temperature('temperatura-max', 'temp_max')
-        self.obj.make_data_source('temp_max')
-        self.assertEqual(expected, self.obj.data_source.data)
+    def test_set_data_source(self):
+        data = mock_loaded_data()
+        expected = {
+            'date_min': data.get('temp_min').get('date'),
+            'temp_min': data.get('temp_min').get('temp_min'),
+            'date_max': data.get('temp_max').get('date'),
+            'temp_max': data.get('temp_max').get('temp_max')
+        }
+        self.obj.load_temperature_data()
+        self.obj.set_data_source()
+        self.assertDictEqual(expected, self.obj.data_source.data)
+
+    def test_set_tools(self):
+        self.obj.set_tools()
+        expected = [
+            ('Temperatura mínima', '@temp_min'),
+            ('Temperatura máxima', '@temp_max'),
+            ('Data', '@date_max{%d/%m/%Y %H:%M}'),
+        ]
+        self.assertListEqual(expected, self.obj.tools.tooltips)
+
+    @mock_api
+    def test_set_plot(self):
+        self.obj.load_temperature_data()
+        self.obj.set_tools()
+        self.obj.set_plot()
+        self.assertIsInstance(self.obj.plot, Figure)
+
+    def test_get_plot(self):
+        plot = self.obj.get_plot()
+        self.assertIsInstance(plot, Figure)
