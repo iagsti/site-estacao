efb0664b30a98618506cc954ece7f39d981ef50a
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-06 14:29:40 -0300

    Adiciona a classe TemperaturePlot - issue #25

--- estacao/core/charts/plot.py
+++ estacao/core/charts/plot.py
@@ -2,5 +2,21 @@ from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, HoverTool, DatetimeTickFormatter
 
 from .data import DataTemperature
+
+PLOT_TITLE = 'Temperatura'
+PLOT_HEIGHT = 300
+X_AXIS_LABEL = 'Data'
+Y_AXIS_LABEL = 'Temperatura'
+TICK_FORMAT = ['%d/%m/%Y %H:%M:%S']
+
+
+class TemperaturePlot():
+    def get_plot(self):
+        self.load_temperature_data()
+        self.set_data_source()
+        self.set_tools()
+        self.set_plot()
+        return self.plot
+
         }
 
