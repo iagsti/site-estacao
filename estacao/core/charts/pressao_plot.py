4f5ae7aa5f17f6ac561e8500268c110055455e2f
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-13 11:09:26 -0300

    Cria o data source para pressao - issue #30
    Adiciona a função set_data_source para criar a o data source utilizado para plotar os gráficos de pressão.

--- estacao/core/charts/pressao_plot.py
+++ estacao/core/charts/pressao_plot.py
@@ -1,5 +1,6 @@
-from .data import DataConsolidado
+from bokeh.models import ColumnDataSource
 
+from .data import DataConsolidado
 
 class PressaoPlot:
     def __init__(self):
@@ -17,3 +18,16 @@ class PressaoPlot:
                 'pressao_hpa': pressao_hpa.extracted_data}
 
         setattr(self, 'data', data)
+
+    def set_data_source(self):
+        data = getattr(self, 'data')
+        pressao = {'date': data.get('date'), 'pressao': data.get('pressao')}
+        pressao_hpa = {'date': data.get('date'),
+                       'pressao_hpa': data.get('pressao_hpa')}
+
+        source_pressao = ColumnDataSource(data=pressao)
+        source_pressao_hpa = ColumnDataSource(data=pressao_hpa)
+        datasource = {'pressao': source_pressao,
+                      'pressao_hpa': source_pressao_hpa}
+
+        setattr(self, 'datasource', datasource)
