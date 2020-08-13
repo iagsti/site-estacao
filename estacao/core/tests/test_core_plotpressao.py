454a9f4c528954f0f40592651baac20a42627428
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-13 11:04:03 -0300

    Adicona mock_data para dados de pressão - issue #30
    Adicona a função mock_data para gerar simular dados de pressão nos testes.

--- estacao/core/tests/test_core_plotpressao.py
+++ estacao/core/tests/test_core_plotpressao.py
@@ -10,13 +10,16 @@ class PressaoPlotTest(TestCase):
 
     @mock_api
     def test_load_data(self):
+        expected = self.mock_data()
+        self.obj.load_data()
+        self.assertEqual(expected, self.obj.data)
+
+    def mock_data(self):
         data = consolidado
         date = [row.get('data') for row in data.get('consolidado')]
         pressao = [row.get('pressao') for row in data.get('consolidado')]
-        pressao_hpa = [row.get('pressao_hpa') for row in data.get('consolidado')]
+        pressao_hpa = [
+            row.get('pressao_hpa') for row in data.get('consolidado')
+        ]
 
-        expected = {'date': date, 'pressao': pressao,
-                    'pressao_hpa': pressao_hpa}
-
-        self.obj.load_data()
-        self.assertEqual(expected, self.obj.data)
+        return {'date': date, 'pressao': pressao, 'pressao_hpa': pressao_hpa}
