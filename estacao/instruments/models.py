918d74c3d38feb73daf490afb6d595e090b0e901
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-31 19:21:19 -0300

    Altera atributo text para TextField - issue #32
    Modificao o tipo do atributo text para TextField.

--- estacao/instruments/models.py
+++ estacao/instruments/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 
 class Instruments(models.Model):
     title = models.CharField(_("título"), max_length=50)
-    text = models.CharField(_("texto"), max_length=2048)
+    text = models.TextField(_("texto"), max_length=2048)
     image = models.FileField(_("imagem"), upload_to='instruments')
 
     def __str__(self):
