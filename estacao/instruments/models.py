a6769471689d9f165fb585dce567f4b4c9b40fa7
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-31 14:53:19 -0300

    Adiciona o model instruments - issue #32
    Adciona o model instruments responsável por representar os intrumentos meteorológicos.

---
+++ estacao/instruments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Instruments(models.Model):
+    title = models.CharField(_("título"), max_length=50)
+    text = models.CharField(_("texto"), max_length=2048)
+    image = models.FileField(_("imagem"), upload_to=None, max_length=100)
