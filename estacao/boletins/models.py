c7745825d49bc5176e7ff7c0d854f14664730ef2
Marcelo Schneider <schneider.fei@gmail.com>
2020-09-08 17:16:13 -0300

    Adiciona model para Boletim - issue #34

--- estacao/boletins/models.py
+++ estacao/boletins/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Boletin(models.Model):
-    pass
+    CATEGORY_CHOICES = (
+        ('climatologico', 'Boletim climatológico'),
+        ('trimestral', 'Boletim trimestral'),
+        ('mensal', 'Boletim mensal'),
+        ('tecnico', 'Relatório técnico'),
+    )
+
+    title = models.CharField(_("Títlo"), max_length=50)
+    category = models.CharField(
+        _("Categoria"), max_length=50, choices=CATEGORY_CHOICES)
+    file = models.FileField(_("Arquivo"), upload_to='boletins')
--- estacao/boletins/tests/test_boletins_models.py
+++ estacao/boletins/tests/test_boletins_models.py
@@ -11,3 +11,36 @@ class BoletinYearlyTest(TestCase):
     def test_instance(self):
         """It should be an instance of Model"""
         self.assertIsInstance(self.obj, models.Model)
+
+    def test_has_attributes(self):
+        attributes = ('title', 'category', 'file')
+
+        for item in attributes:
+            with self.subTest():
+                message = '{} not found'.format(item)
+                self.assertTrue(hasattr(self.obj, item), msg=message)
+
+    def test_attributes_type(self):
+        attributes = (
+            ('title', models.CharField),
+            ('category', models.CharField),
+            ('file', models.FileField)
+        )
+
+        for item, instance in attributes:
+            with self.subTest():
+                self.assertIsInstance(getattr(Boletin, item).field, instance)
+
+    def test_category_choices(self):
+        category = Boletin.category.field.choices
+        expected = (
+            ('climatologico', 'Boletim climatológico'),
+            ('trimestral', 'Boletim trimestral'),
+            ('mensal', 'Boletim mensal'),
+            ('tecnico', 'Relatório técnico')
+        )
+        self.assertTupleEqual(expected, category)
+
+    def test_file_uploa_to_attribute(self):
+        upload_to = Boletin.file.field.upload_to
+        self.assertEqual(upload_to, 'boletins')
