eafab042a4b1ebffbccf8d5a5b37a33fd9293154
Marcelo Schneider <schneider.fei@gmail.com>
2020-09-08 22:35:38 -0300

    Adiciona context na view Boletin - issue #34
    Adiciona os parâmetros de contexto para a view Boletin

--- estacao/boletins/views.py
+++ estacao/boletins/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Boletin
+
 
 def boletins_index(request):
-    context = {'boletins': {}}
+    context = {
+        'climatologico': Boletin.objects.climatologico(),
+        'trimestral': Boletin.objects.trimestral(),
+        'mensal': Boletin.objects.mensal(),
+        'tecnico': Boletin.objects.tecnico()
+    }
     return render(request, 'boletins.html', context)
