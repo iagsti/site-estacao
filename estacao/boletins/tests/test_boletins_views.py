8f05bf4f413aeb59c84fae4292a60225adf56bd5
Marcelo Schneider <schneider.fei@gmail.com>
2020-09-08 22:38:53 -0300

    Renderiza boletins - issue #34
    Renderiza os dados de boletins.

--- estacao/boletins/tests/test_boletins_views.py
+++ estacao/boletins/tests/test_boletins_views.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
+from ..models import Boletin
+
 
 class BoletinsGetTest(TestCase):
     def setUp(self):
+        Boletin.objects.create(title='Boletim climatológico',
+                               category='climatologico', file='boletim.pdf')
+        Boletin.objects.create(title='Boletim trimestral',
+                               category='trimestral', file='boletim.pdf')
+        Boletin.objects.create(title='Boletim mensal',
+                               category='mensal', file='boletim.pdf')
+        Boletin.objects.create(title='Boletim técnico',
+                               category='tecnico', file='boletim.pdf')
+
         self.resp = self.client.get(r('boletins:index'))
 
     def test_status_code(self):
@@ -24,6 +35,14 @@ class BoletinsGetTest(TestCase):
 
     def test_context_has_tecnico(self):
         self.assertContextHasMember('tecnico')
+
+    def test_template_contains_boletin_data(self):
+        content = ('Boletim climatológico', 'Boletim trimestral',
+                   'Boletim mensal', 'Boletim técnico')
+
+        for item in content:
+            with self.subTest():
+                self.assertContains(self.resp, item)
 
     def assertContextHasMember(self, member):
         context = self.resp.context
